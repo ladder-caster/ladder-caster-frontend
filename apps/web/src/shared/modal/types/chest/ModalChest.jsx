428ba12d26a94f75c6bfe30add665fbb04001c6f
John Daffue <john.daffue@vodafone.com>
2022-04-28 20:34:50 +0100

    chest modal and styling

--- apps/web/src/shared/modal/types/chest/ModalChest.jsx
+++ apps/web/src/shared/modal/types/chest/ModalChest.jsx
@@ -1,52 +1,77 @@
 import React, { useRef } from 'react';
-import { _chest, _image, _confirm } from './ModalChest.styled';
+import { _grid, _gridItem, _gridItemSelectable, _gridContainer, _gridLabel,_gridCloseButton } from './ModalChest.styled';
 import { useTranslation } from 'react-i18next';
 import { useActions } from '../../../../../actions';
 import { useClickOutside } from 'core/hooks/useClickOutside';
-import { AnimateButton } from '../../../../views/game/nav/animations/AnimateButton';
-import { _button } from '../loot/ModalLoot.styled';
+
 import {
   GAME_CONFIRM,
   GAME_INVENTORY,
   ITEM_CHEST,
   MODAL_ACTIVE,
+  TIER_I,
+  TIER_II,
+  TIER_III,
+  TIER_IV
 } from 'core/remix/state';
 import { useRemix } from 'core/hooks/remix/useRemix';
-import { filter } from 'lodash';
+import { filter, clamp } from 'lodash';
 import NFT from '../../../nft/NFT';
-
+import { _level, _overlay } from '../../../item/Item.styled';
 const ModalChest = () => {
-  const { t } = useTranslation();
+  //const { t } = useTranslation();
   const { modalClear, confirmChest } = useActions();
   const [modal] = useRemix(MODAL_ACTIVE);
   const button_ref = useRef();
+  const chest_ref = useRef();
   const [inventory] = useRemix(GAME_INVENTORY);
-  const [confirm] = useRemix(GAME_CONFIRM);
-  const first_chest = filter(
-    inventory?.chests,
-    (chest) => chest.tier === modal?.tier,
-  )?.[0];
+  const chests = filter(inventory?.chests,
+    (chest) => chest.tier === modal?.tier).sort((a,b)=>a.level-b.level);
+  //const [confirm] = useRemix(GAME_CONFIRM);
 
-  useClickOutside(button_ref, () => modalClear());
-
+  const tierMap = {
+    [TIER_I]: 1,
+    [TIER_II]: 2,
+    [TIER_III]: 3,
+    [TIER_IV]: 4,
+  }
+  const chestTier = tierMap[modal?.tier];
+  const columnCount = 4;
+  const chestPlaceHolderCount = 20;
+  var chestEmptyLimit = chests?.length>chestPlaceHolderCount? chests?.length % columnCount:clamp(chestPlaceHolderCount - chests?.length, 0, chestPlaceHolderCount);
+  // fill remaining empty slots in the row
+  if(chests?.length>0 && chestEmptyLimit!=0) chestEmptyLimit = columnCount-chestEmptyLimit;
+  useClickOutside([button_ref, chest_ref], () => modalClear());
   return (
-    <_chest>
-      <_image onClick={() => confirmChest(first_chest?.mint || first_chest)}>
-        {first_chest && <NFT type={ITEM_CHEST} tier={modal?.tier} />}
-      </_image>
-      <_confirm>
-        <AnimateButton $hidden={!confirm} key={'button-modal-chest'}>
-          <_button
-            disabled={!confirm}
-            key={'button-modal-chest'}
-            ref={button_ref}
-            onClick={() => confirmChest(first_chest?.mint || first_chest)}
-          >
-            {t('modal.chest.action')}
-          </_button>
-        </AnimateButton>
-      </_confirm>
-    </_chest>
+    <_gridContainer>
+      <_gridLabel>{modal?.tier?.split("_")[0]} {chestTier}</_gridLabel>
+      <_gridCloseButton><span>close</span></_gridCloseButton>
+      <_grid ref={chest_ref}>
+        {
+          chests.map((chest, index) => {
+            return (
+              <_gridItemSelectable key={'chest_modal_' + index} onClick={() => { console.log(chest); confirmChest(chest?.mint || chest) }}>
+                <_overlay>
+                  <_level>
+                    <span>{chest.level}</span>
+                  </_level>
+                </_overlay>
+
+                <NFT
+                  type={ITEM_CHEST}
+                  tier={chest.tier}
+                />
+
+              </_gridItemSelectable>
+            )
+          })
+        }
+        {
+          [...Array(chestEmptyLimit).keys()].map((x) =>
+            <_gridItem key={'chest_non_selectable_' + x} />)
+        }
+      </_grid>
+    </_gridContainer>
   );
 };
 
--- apps/web/src/shared/modal/types/chest/ModalChest.styled.js
+++ apps/web/src/shared/modal/types/chest/ModalChest.styled.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
 export const _chest = styled.div`
   width: 100%;
@@ -23,3 +23,109 @@ export const _image = styled.div`
 `;
 
 export const _confirm = styled.div``;
+export const _gridContainer = styled(_chest)`
+  margin-top: 10rem;
+  height:100%;
+  postion:relative;
+  top: 0;
+  backdrop-filter: blur(5px);
+`;
+export const _grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+
+  grid-gap: 0.5rem;
+  overflow-y: scroll;
+  height:100%;
+  width:100%;
+  background:#1f2437;
+  border-radius: 1rem 1rem 0 0 ;
+  align-items: center;
+  position: relative;
+  top:0;
+  padding: 1.25rem 1rem 1.25rem 1rem;
+  margin-top: 1rem;
+  margin-bottom: 4.5rem;
+
+  @media (max-width: 768px) {
+    height: 90%;
+    width:97%;
+  }
+  @media (max-width: 356px) {
+    height: 75%;
+  }
+`;
+
+export const _gridLabel = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+  font-family: 'Poppins';
+  font-style: normal;
+  color: #8692b1;
+  letter-spacing: 0.2rem;
+
+`;
+const chestAnimation = keyframes`
+  from {
+    transform:scale(0.95);
+    filter: drop-shadow(0 0 0.1rem gold);
+  }
+  to{
+    transform:scale(1.05);
+    filter: drop-shadow(0 0 .3rem gold);
+  }
+`;
+export const _gridItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: ${props => props?.color || '#1a1e2d'};
+  border-radius: 0.5rem;
+  box-shadow: inset 0 0 0.5rem #000000;
+  aspect-ratio: 1;
+  min-width:81px;
+  
+  @media (max-width: 400px) {
+    min-width:30px;
+  }
+`;
+
+export const _gridItemSelectable = styled(_gridItem)`
+cursor:pointer;
+transition: box-shadow 0.5s, transform 0.5s, filter 0.33s ease-in-out;
+> img {
+  transform: scale(.95,.95);
+}
+> div > div {
+  border-radius: 8px 0 8px 0 ;
+  background-color: #8692b1;
+}
+&:hover  {
+  transform: scale(1.1,1.1);
+  filter: drop-shadow(0 0 .1rem white);
+  > img {
+    animation: ${chestAnimation} 1s ease-in-out infinite alternate;
+  }
+}
+`;
+export const _gridCloseButton = styled.div`
+display: flex;
+position: absolute;
+top: 2rem;
+right: 2rem;
+color: #cbcfe2;
+background-color:#1f2437;
+border-radius: 0.5rem 0.5rem 0 0 ;
+width: 3rem;
+height: 2rem;
+align-items: center;
+justify-content: center;
+text-align: center;
+margin: 0 auto;
+transition: box-shadow 0.25s, transform 0.2s ease-in-out;
+&:hover {
+  box-shadow:  0 0 .5rem white;
+  transform: scale(1.05);
+}
+`;
