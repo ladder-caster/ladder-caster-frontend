71be0c32db460c788a70a065994c1437becbeabc
deadeagle63 <56412825+deadeagle63@users.noreply.github.com>
2022-05-01 15:05:56 +0100

    fixed display and visibility

--- apps/web/src/shared/modal/types/chest/ModalChest.jsx
+++ apps/web/src/shared/modal/types/chest/ModalChest.jsx
@@ -1,5 +1,13 @@
 import React, { useRef } from 'react';
-import { _grid, _grid_item, _grid_item_selectable, _grid_container, _grid_label,_grid_close_button,_double_height_clickable } from './ModalChest.styled';
+import {
+  _grid,
+  _grid_item,
+  _grid_item_selectable,
+  _grid_container,
+  _grid_label,
+  _grid_close_button,
+  _double_height_clickable,
+} from './ModalChest.styled';
 import { useTranslation } from 'react-i18next';
 import { useActions } from '../../../../../actions';
 import { useClickOutside } from 'core/hooks/useClickOutside';
@@ -12,7 +20,7 @@ import {
   TIER_I,
   TIER_II,
   TIER_III,
-  TIER_IV
+  TIER_IV,
 } from 'core/remix/state';
 import { useRemix } from 'core/hooks/remix/useRemix';
 import { filter, clamp } from 'lodash';
@@ -24,8 +32,10 @@ const ModalChest = () => {
   const [modal] = useRemix(MODAL_ACTIVE);
   const grid_ref = useRef();
   const [inventory] = useRemix(GAME_INVENTORY);
-  const chests = filter(inventory?.chests,
-    (chest) => chest.tier === modal?.tier).sort((a,b)=>b?.level-a?.level);
+  const chests = filter(
+    inventory?.chests,
+    (chest) => chest.tier === modal?.tier,
+  ).sort((a, b) => b?.level - a?.level);
   //const [confirm] = useRemix(GAME_CONFIRM);
 
   const tierMap = {
@@ -33,45 +43,52 @@ const ModalChest = () => {
     [TIER_II]: 2,
     [TIER_III]: 3,
     [TIER_IV]: 4,
-  }
+  };
   const columnCount = 4;
   const chestPlaceHolderCount = 20;
-  let chestEmptyLimit = chests?.length && chests?.length > chestPlaceHolderCount ? chests?.length % columnCount : clamp(chestPlaceHolderCount - chests?.length, 0, chestPlaceHolderCount);
+  let chestEmptyLimit =
+    chests?.length && chests?.length > chestPlaceHolderCount
+      ? chests?.length % columnCount
+      : clamp(chestPlaceHolderCount - chests?.length, 0, chestPlaceHolderCount);
   // fill remaining empty slots in the row
-  if (chests?.length > 0 && chestEmptyLimit !== 0) chestEmptyLimit = columnCount - chestEmptyLimit >= 0 ? columnCount - chestEmptyLimit : 0;
+  if (chests?.length > 0 && chestEmptyLimit !== 0)
+    chestEmptyLimit =
+      columnCount - chestEmptyLimit >= 0 ? columnCount - chestEmptyLimit : 0;
   useClickOutside([grid_ref], () => modalClear());
-  const emptyGrid = chestEmptyLimit >= 0 ? Array(chestEmptyLimit).keys() : [];
+  const emptyGrid = chestEmptyLimit > 0 ? Array(chestEmptyLimit).keys() : [];
   const gridMap = [...chests, ...emptyGrid];
 
   return (
     <_grid_container>
-      <_grid_label>{t("modal.chests.title").toUpperCase()} {tierMap[modal?.tier]}</_grid_label>
-      <_grid ref={grid_ref}>
-        {
-          gridMap.map((chest,index)=>{
-            if(chest?.tier){
+      <_grid_label>
+        {t('modal.chests.title').toUpperCase()} {tierMap[modal?.tier]}
+      </_grid_label>
+      <_grid ref={grid_ref} rows={Math.floor(gridMap.length / 4)}>
+        {gridMap.map((chest, index) => {
+          if (chest?.tier) {
             return (
-              <_grid_item_selectable key={'chest_full_' + index} onClick={() => confirmChest(chest?.mint || chest)}>
-              <_overlay>
-                <_level>
-                  <span>{chest.level}</span>
-                </_level>
-              </_overlay>
+              <_grid_item_selectable
+                key={'chest_full_' + index}
+                onClick={() => confirmChest(chest?.mint || chest)}
+              >
+                <_overlay>
+                  <_level>
+                    <span>{chest.level}</span>
+                  </_level>
+                </_overlay>
 
-              <NFT
-                type={ITEM_CHEST}
-                tier={chest.tier}
-              />
-
-            </_grid_item_selectable>
-            )}
-            
-            return <_grid_item key={'chest_empty_'+index}></_grid_item>;
+                <NFT type={ITEM_CHEST} tier={chest.tier} />
+              </_grid_item_selectable>
+            );
           }
-          )
-        }
+
+          return <_grid_item key={'chest_empty_' + index}></_grid_item>;
+        })}
       </_grid>
-      <_grid_close_button><span>{t("drawer.close")}</span><_double_height_clickable/></_grid_close_button>
+      <_grid_close_button>
+        <span>{t('drawer.close')}</span>
+        <_double_height_clickable />
+      </_grid_close_button>
     </_grid_container>
   );
 };
--- apps/web/src/shared/modal/types/chest/ModalChest.styled.js
+++ apps/web/src/shared/modal/types/chest/ModalChest.styled.js
@@ -1,4 +1,4 @@
-import styled, {keyframes} from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const _chest = styled.div`
   width: 100%;
@@ -25,24 +25,26 @@ export const _image = styled.div`
 export const _confirm = styled.div``;
 export const _grid_container = styled(_chest)`
   margin-top: 8px;
-  height:100%;
+  height: 100%;
   top: 0;
   backdrop-filter: blur(5px);
   padding-top: 72px;
-  padding-bottom: 12px;
 `;
 export const _grid = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(
+    ${(props) => (props.rows > 4 ? props.rows : 4)},
+    1fr
+  );
   grid-gap: 8px;
   overflow-y: scroll;
-  height:100%;
-  width:100%;
+  height: 100%;
+  width: 100%;
   align-items: center;
   position: relative;
-  top:0;
-  padding: 16px 16px 0px 16px;
-
+  top: 0;
+  padding: 16px 16px 16px 16px;
 `;
 
 export const _grid_label = styled.div`
@@ -56,11 +58,12 @@ export const _grid_label = styled.div`
 const _chest_animation = keyframes`
   from {
     transform:scale(1.08);
-    filter: drop-shadow(0 0 0.1rem ${({ theme }) => theme.text.alert});
+    filter: drop-shadow(0 0 0.1rem ${({ theme }) =>
+      theme.text.alert}) brightness(1.08);
   }
   to{
     transform:scale(1.2);
-    filter: drop-shadow(0 0 .2rem gold);
+    filter: drop-shadow(0 0 .2rem gold) brightness(1.2);;
     z-index: 999;
   }
 `;
@@ -70,60 +73,60 @@ export const _grid_item = styled.div`
   align-items: center;
   justify-content: center;
   aspect-ratio: 1;
-  min-width:16px;
+  min-width: 16px;
   filter: brightness(1.05);
 `;
 
 export const _grid_item_selectable = styled(_grid_item)`
-filter: brightness(1.08);
-cursor:pointer;
-transition: box-shadow 0.5s, transform 0.5s, filter 0.33s ease-in-out;
-> img {
-  transform: scale(1.1);
-}
-> div > div {
-  border-radius: 50%;
-  background-color: ${({ theme }) => theme.exp.texture};
-  box-shadow: ${({ theme }) => theme.shadow['popover']};
-}
-&:hover, &:focus  {
-  
-  filter:  brightness(1.2);
+  filter: brightness(1.08);
+  cursor: pointer;
+  transition: box-shadow 0.5s, transform 0.5s, filter 0.33s ease-in-out;
   > img {
-    transform: scale(1.2,1.2);
-    
-    animation: ${_chest_animation} 2s cubic-bezier(.84,-0.86,.36,1.62) infinite alternate;
+    transform: scale(1.1);
   }
-  
-}
+  > div > div {
+    border-radius: 50%;
+    background-color: ${({ theme }) => theme.item['legendary']};
+    box-shadow: ${({ theme }) => theme.shadow['popover']};
+  }
+  &:hover,
+  &:focus {
+    > img {
+      transform: scale(1.2, 1.2);
+
+      animation: ${_chest_animation} 2s cubic-bezier(0.84, -0.86, 0.36, 1.62)
+        infinite alternate;
+    }
+  }
 `;
 export const _grid_close_button = styled.div`
-position:relative;
-display: flex;
-color: ${({ theme }) => theme.text.active};
-background-color: ${({ theme }) => theme.background.lowest};
-border-radius: 16px 16px 0 0;
-width: 100%;
-height: 48px;
-align-items: center;
-justify-content: center;
-text-align: center;
-margin: 0 auto;
-transition: box-shadow 0.25s ease-in-out;
-cursor: pointer;
-&:hover, &:focus {
-  box-shadow:  ${({ theme }) => theme.shadow['frost']};
-  background: ${({ theme }) => theme.background['button_low']};
-}
-&:active {
-  background-color: #1a1e2d;
-}
+  position: relative;
+  display: flex;
+  color: ${({ theme }) => theme.text.active};
+  background-color: ${({ theme }) => theme.background.lowest};
+  border-radius: 16px 16px 0 0;
+  width: 100%;
+  height: 48px;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  margin: 0 auto;
+  transition: box-shadow 0.25s ease-in-out;
+  cursor: pointer;
+  &:hover,
+  &:focus {
+    box-shadow: ${({ theme }) => theme.shadow['frost']};
+    background: ${({ theme }) => theme.background['button_low']};
+  }
+  &:active {
+    background-color: #1a1e2d;
+  }
 `;
 export const _double_height_clickable = styled.div`
-@media (max-width: 400px) {
-  cursor: pointer;
-  width: 100%;
-  height: 200%;
-  position:absolute;
-}
+  @media (max-width: 400px) {
+    cursor: pointer;
+    width: 100%;
+    height: 200%;
+    position: absolute;
+  }
 `;
