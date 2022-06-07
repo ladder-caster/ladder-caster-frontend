8de90631d0939e88b0259796d1be57a33fe91161
Danny M <danny.manzano.172@gmail.com>
2022-06-07 16:35:25 -0400

    Fix toggle button

--- apps/web/src/shared/button/toggle/Toggle.jsx
+++ apps/web/src/shared/button/toggle/Toggle.jsx
@@ -2,18 +2,17 @@ import { motion } from 'framer-motion';
 import React from 'react';
 import { _container, _toggle_background, _toggle_orb } from './Toggle.styled';
 function Toggle({ onClick, checked, scale }) {
-
   return (
-    <_container onClick={onClick} $scale={scale}>
-      <_toggle_background checked={checked} >
+    <_container onClick={onClick}>
+      <_toggle_background checked={checked}>
         <_toggle_orb
           checked={checked}
           as={motion.div}
           animate={{
-            right: checked ? '-40%' : '40%',
+            right: checked ? -10 : 10,
           }}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
+          // whileHover={{ scale: 1.1 }}
+          // whileTap={{ scale: 0.9 }}
         />
       </_toggle_background>
     </_container>
