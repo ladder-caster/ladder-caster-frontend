9c4c57bca51f31707875a68b2530495d3f6afcb4
deadeagle63 <56412825+deadeagle63@users.noreply.github.com>
2022-07-24 20:37:25 +0100

    useAction
    use action cache,
    as the config needs a functional component - needed to abstract it one layer further to allow for a single load instead of each time

--- apps/web/actions/index.js
+++ apps/web/actions/index.js
@@ -1,13 +1,5 @@
-import { useConnections } from './useConnections';
-import { useInstructions } from './useInstructions';
-import { useStateActions } from './useStateActions';
-import { useTesters } from './useTesters';
+import { loadedActions } from 'core/actions';
 
 export const useActions = () => {
-  const connections = useConnections();
-  const testers = useTesters();
-  const stateActions = useStateActions();
-  const instructions = useInstructions();
-
-  return { ...connections, ...testers, ...stateActions, ...instructions };
+  return loadedActions;
 };
