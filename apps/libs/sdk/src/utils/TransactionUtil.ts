381932bf37bc0337b95eee3644f3d9a949fea95e
Danny M <danny.manzano.172@gmail.com>
2023-04-05 17:46:20 -0400

    Fix claim all and fallback claim

--- apps/libs/sdk/src/utils/TransactionUtil.ts
+++ apps/libs/sdk/src/utils/TransactionUtil.ts
@@ -20,11 +20,58 @@ export const signTransaction = async (
   return await wallet.signTransaction(builder.transaction);
 };
 
+export const signAllTransaction = async (
+  builders: TransactionBuilder[],
+  blockhash: string,
+  wallet: NodeWallet,
+) => {
+  const transactions: Transaction[] = [];
+  for (const builder of builders) {
+    builder.transaction.recentBlockhash = blockhash;
+    builder.transaction.feePayer = wallet.publicKey;
+    for (const sign of builder.signers || []) {
+      builder.transaction.partialSign(sign);
+    }
+
+    transactions.push(builder.transaction);
+  }
+  return await wallet.signAllTransactions(transactions);
+};
+
 export const executeTransaction = async (
   connection: Connection,
-  signedTx: Transaction,
+  signedTxs: Transaction[],
+  atomicTransactions: boolean = false,
 ) => {
-  return await connection.sendRawTransaction(signedTx.serialize());
+  let validatorSignatures: string[] = [];
+
+  if (atomicTransactions) {
+    for (const signedTx of signedTxs) {
+      validatorSignatures.push(
+        await connection.sendRawTransaction(signedTx.serialize()),
+      );
+    }
+
+    if (validatorSignatures.length !== signedTxs.length && atomicTransactions)
+      throw 'Some transactions failed to execute';
+  } else {
+    const executionPromise = [];
+    for (const signedTx of signedTxs) {
+      executionPromise.push(
+        connection.sendRawTransaction(signedTx.serialize()),
+      );
+    }
+
+    const results = await Promise.allSettled(executionPromise);
+
+    results.forEach((result) => {
+      if (result.status === 'fulfilled') {
+        validatorSignatures.push(result.value);
+      }
+    });
+  }
+
+  return validatorSignatures;
 };
 
 export const confirmTransaction = async (
